7071f90cb4803d2baf8edf706bfa2f7e29f5d775
kjbenk <kjbenk@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-08-02 15:57:25 +0000

    Fixed bug that doesnt allow for automatic plugin install
    git-svn-id: https://plugins.svn.wordpress.org/show-stock-quotes/trunk@750064 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- show_stock_quotes/index.php
+++ show_stock_quotes/index.php
@@ -1,25 +1,11 @@
 <?php
 /*
-	Plugin Name: Gabfire Social Mashup Widget
-	Plugin URI: http://www.gabfirethemes.com
-	Description: Display a mashup of specified social platforms in a sidebar.
-	Author: Kyle Benk
-	Version: 1.0
-	Author URI: http://www.kylebenk.com
-	
-	Copyright 2013 Gabfire Themes (email : info@gabfire.com)
-	
-    This program is free software; you can redistribute it and/or modify
-    it under the terms of the GNU General Public License, version 2, as 
-    published by the Free Software Foundation.
-
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
-
-    You should have received a copy of the GNU General Public License
-    along with this program; if not, write to the Free Software
-    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA 
+Plugin Name: Show Stock Quotes
+Plugin URI: http://kylebenkapps.com/wordpress-plugins/
+Description: Show stock quotes updated in real-time.
+Version: 1.0
+Author: Kyle Benk
+Author URI: http://kylebenkapps.com
+License: GPL2
 */
 ?>
